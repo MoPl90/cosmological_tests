e98d2d64430ba90664c9d5f5aa374f603d33400d
kma-code <dankemax@web.de>
2019-07-24 16:01:19 +0200

    On branch BAO  Changes to be committed: 	modified:   Cosmology.py 	modified:   Sample_Cosmo.ipynb 	modified:   data/bao_1806-06781.txt 	modified:   plots/Hubble_LCDM.pdf 	modified:   plots/posterior_LCDM.pdf
    Fully implemented analysis of BAO data: can calculate sound horizon, distance modulus, likelihood.
    
    Added omega_r to arguments of cosmology (required for calculation of sound horizon).
    
    Added autoreload lib: changing cosmology.py does not require restarting kernel anymore. Watch out, this sometimes breaks evaluation.
    
    Changed handling of data and errors in the cosmology class. This now takes a data object (data, error) as an argument.

--- Cosmology.py
+++ Cosmology.py
@@ -11,15 +11,16 @@ class cosmology:
     """This class implements the base class for any cosmological model we study. More involved models inherit from this class."""
     
     cLight = 3E5 # speed of light in km/s
-    H0 = 70. #the present day Hubble rate in km/s/Mps
+    #H0 = 70. #the present day Hubble rate in km/s/Mps
     
-    def __init__(self, omegam, omegar, omegac, w=-1):
+    def __init__(self, omegam, omegar, omegac, w=-1, Hzero=70):
         """Initialise a cosmological model"""
         self.Omegam = omegam #non-relativistic matter energy density
         self.Omegar = omegar #relativistic matter energy density
         self.Omegac = omegac #dark energy density
         self.Omegak = 1 - omegam - omegac #curvature energy density
         self.eos = w #the dark energy equation of state
+        self.H0 = Hzero #the present day Hubble rate in km/s/Mps
         
     def set_energy_densities(self, omegam, omegac):
         """Change the initialised values of Omega_i."""
@@ -74,8 +75,24 @@ class cosmology:
         return 5*(np.log10(np.abs(self.luminosity_distance(z))) + 5)
     
     
-    def log_likelihood(self, data, Cov):
+    def log_likelihood(self, dataObject):
         """Compute the Gaussian log-likelihood for given data tuples (z, DM(z)) and covariance."""
+        
+        
+        
+  
+        if dataObject.name == 'Quasars' or dataObject.name == 'SN':
+            data = dataObject.distance_modulus()
+            Cov = dataObject.delta_distance_modulus()
+        elif dataObject.name == 'BAO':
+            data = dataObject.distance_modulus(self)
+            Cov = dataObject.delta_distance_modulus(self)
+        else:
+            raise ValueError('Data type needs to be associated with input (e.g. BAO, quasars, ...)')
+            
+        
+  
+        # bring data into correct shape in case it isnt:
         
         if data.shape[0] == 2:
             z = data[0]
@@ -86,7 +103,25 @@ class cosmology:
         else:
             raise ValueError('Data has wrong format.')
             
+
+ 
+        #if dataObject.name == 'Quasars' or dataObject.name == 'SN':
+            #data = dataObject.distance_modulus()
+            #Cov = dataObject.delta_distance_modulus()
+        #elif dataObject.name == 'BAO':
+            #data = dataObject.distance_modulus(self)
+            #Cov = 1
+       # else:
+            #raise ValueError('Data type needs to be associated with input (e.g. BAO, quasuars, ...)')
+ 
+ 
+        # define the model DM:
+        
         model = self.distance_modulus(z)
+        
+
+        
+  
         
 
         if len(Cov.shape) == 2:
@@ -115,6 +150,7 @@ class Supernova_data:
             self.data = data   
             self.err = err   
             self.param = param
+            self.name = "SN"
         
     def get_data(self):
         return self.data
@@ -162,6 +198,7 @@ class Quasar_data:
             self.data = data   
             self.err = err   
             self.param = param
+            self.name = "Quasars"
             
     def get_data(self):
         return self.data
@@ -206,15 +243,22 @@ class BAO_data:
     """Objects of this class represent BAO data sets."""
     
     cLight = 3E5 # speed of light in km/s
+    z_d = 1089   # redshift of decoupling
     
     
-    def __init__(self, data, err, param):
+    def __init__(self, data, err, param, dataType):
         if data.shape[1] != 2 and err.shape[1] != 1:
             raise ValueError('Data has wrong format: data = (z, DM/rd)')
         else:
             self.data = data   
             self.err = err   
             self.param = param
+            self.dataType = dataType
+            self.name = "BAO"
+            
+            # take average of errors if not symmetrical
+            if self.err.shape[1] == 2:
+                self.err = (self.err[:, 0]+self.err[:, 1])/2.
             
     def get_data(self):
         return self.data
@@ -239,22 +283,62 @@ class BAO_data:
         return comsoundhorizon
  
          
- #   def distance_modulus(self):
- #       beta_prime = self.param[0]
- #       z, logLUV, logLX, logFUV, logFX = self.data.T
- #   
- #       DM_Q = 5 / 2 / (self.gamma-1) * (logFX - self.gamma * logFUV + beta_prime)
+    def distance_modulus(self,cosmo):
+        # where our heroes convert all BAO data (which come from a variety of formats) into
+        # the standardised dist mod
         
- #       return np.array([z, DM_Q]).T
+        z, meas = self.data.T
+        dtype = self.dataType
         
- #   def delta_distance_modulus(self):
- #       s = self.param[1]
- #       z = self.data.T[0]
- #       err_logFX = self.err
-       
-#        sigmaQ = np.sqrt((5/2/(1-self.gamma) * err_logFX)**2 + s**2)
+        rd_fid = 147.78 # fiducial sound horizon in MPc
+        z_d = self.z_d
         
-#        return sigmaQ
+        rd = self.com_sound_horizon(z_d,cosmo) # sound horizon for given cosmology
+        
+        DMpairs = np.empty([len(dtype), 2])
+        
+        for line in range(0,len(dtype)): 
+            if dtype[line]=='DM*rd_fid/rd':
+                # calculate lumi dist and DM
+                lumiDist = (1 + z[line]) * meas[line]*rd/rd_fid
+                DMpairs[line] = ( z[line], 5*(np.log10(lumiDist) + 5) )
+                
+            elif dtype[line]=='rd/DV':
+                lumiDist =  (1 + z[line]) * ( (rd/meas[line])**3 * cosmo.H(z[line])/self.cLight/z[line] )**(1/2)
+                DMpairs[line] =  ( z[line], 5*(np.log10(lumiDist) + 5) )             
+                
+            else:
+                raise ValueError('input data doesn\'t have a recognised format')
+
+        return DMpairs
+        
+    def delta_distance_modulus(self,cosmo):
+
+        z, meas = self.data.T
+        dtype = self.dataType
+        
+        rd_fid = 147.78 # fiducial sound horizon in MPc
+        z_d = self.z_d
+        
+        rd = self.com_sound_horizon(z_d,cosmo) # sound horizon for given cosmology
+        
+        sigmaDM = np.empty([len(dtype), 1])
+                
+        for line in range(0,len(dtype)): 
+            if dtype[line]=='DM*rd_fid/rd':
+                # calculate com dist and DM:
+                siglumiDist = (1 + z[line]) * self.err[line]*rd/rd_fid
+                sigmaDM[line] =  5*siglumiDist/meas[line]
+                
+            elif dtype[line]=='rd/DV':
+                sigmaDM[line] =  5*3/2*self.err[line]/meas[line]
+                
+            else:
+                raise ValueError('input data doesn\'t have a recognised format')
+        
+        return sigmaDM.T[0]
+
+
 
 
 
